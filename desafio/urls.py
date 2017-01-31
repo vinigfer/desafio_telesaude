9a0b30dc47d86137557249b00f6f20e9a8674124
Vinicius Ferreira <vini.g.fer@gmail.com>
2017-01-31 03:54:41 -0200

    - Alteradas rotas para o CRUD do Solicitante

--- desafio/urls.py
+++ desafio/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, url
 from desafio import views
 
 urlpatterns = [
-    url(r'^$', views.solicitante_list, name='solicitante_list'),
-    url(r'^new$', views.solicitante_create, name='solicitante_new'),
-    url(r'^edit/(?P<pk>\d+)$', views.solicitante_update, name='solicitante_edit'),
-    url(r'^delete/(?P<pk>\d+)$', views.solicitante_delete, name='solicitante_delete'),
+    url(r'^solicitante$', views.solicitante_list, name='solicitante_list'),
+    url(r'^solicitante_new$', views.solicitante_create, name='solicitante_new'),
+    url(r'^solicitante_edit/(?P<pk>\d+)$', views.solicitante_update, name='solicitante_edit'),
+    url(r'^solicitante_delete/(?P<pk>\d+)$', views.solicitante_delete, name='solicitante_delete'),
 ]
