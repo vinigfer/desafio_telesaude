38d1f2163471e5c72f1b5d9403dae2e0c862b42c
Vinicius Ferreira <vini.g.fer@gmail.com>
2017-02-03 10:56:21 -0200

    - Primeiro teste desenvolvido

--- desafio/tests.py
+++ desafio/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+from .models import Solicitante
+
+
+class SolicitanteTests(TestCase):
+    def test_read_after_inserting_with_zero_solicitantes(self):
+        Solicitante.objects.create(nome="Jose", email="jose@email.com",
+                                   cpf="01022233345", telefone="5133337777")
+        response = self.client.get(reverse('solicitante_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Jose")
+        self.assertQuerysetEqual(response.context['object_list'],
+                                 ['<Solicitante: Jose (01022233345)>'])
+
+
