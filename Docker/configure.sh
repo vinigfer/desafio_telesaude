11814c36541cce43b269e3af0f33988805f61077
Vinicius Ferreira <vini.g.fer@gmail.com>
2017-02-02 22:26:51 -0200

    - Migrations movidas de lugar. Sao executadas ao iniciar o container - Adicionado sleep antes de subir container web (esperar container database ficar pronto)

--- Docker/configure.sh
+++ Docker/configure.sh
@@ -4,4 +4,3 @@ apt-get install -y python3 python3-pip git-core libpq-dev postgresql-client
 git clone https://github.com/vinigfer/desafio_telesaude.git
 cd desafio_telesaude
 pip3 install -r requirements.txt
-python3 manage.py migrate
