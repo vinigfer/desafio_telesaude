1a8b0fa66121df3b770363c60313dfab5c9d76a7
Vinicius Ferreira <vini.g.fer@gmail.com>
2017-02-05 13:03:00 -0200

    - Alterados os forms de solicitacao e solicitante para usar bootstrap - Alterado idioma do Projeto

--- telesaude/settings.py
+++ telesaude/settings.py
@@ -95,7 +95,7 @@ DATABASES['default'].update(db_from_env)
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
