58702850d2c8a14e783cd7b95ff4ea006665f43e
Vinicius Ferreira <vini.g.fer@gmail.com>
2017-02-06 00:36:38 -0200

    - Adicionado arquivo para registrar comandos uteis do Django - Ajustes na instalacao local

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 sudo apt-get update
 sudo apt-get install -y python3 python3-pip git-core libpq-dev postgresql postgresql-client
 sudo pip3 install -r requirements.txt
+sudo sed -i '1 i\127.0.0.1      database' /etc/hosts
+sudo -u postgres psql -c "CREATE USER usuario WITH PASSWORD 'senha';"
+sudo -u postgres psql -c "CREATE DATABASE telesaude;"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE telesaude TO usuario;"
+python3 manage.py migrate
